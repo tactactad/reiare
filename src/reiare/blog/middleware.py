ac3f03b5c1a278b5ecc54368fec0faea1b2006fc
Takuro Shimoda <tac@reiare.net>
2011-06-07 22:32:08 +0900

    add 3DS to smart phone list
    リダイレクト対象に3DSを追加した。普通のページでは読み込みエラーが出たため。

--- src/reiare/blog/middleware.py
+++ src/reiare/blog/middleware.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 class RedirectSmartPhoneMiddleware(object):
     def process_request(self, request):
         if request.META.has_key('HTTP_USER_AGENT') and \
-                re.search('iPod|iPhone|Android|BlackBerry|Windows Phone|Symbian',
+                re.search('iPod|iPhone|Android|BlackBerry|Windows Phone|Symbian|Nintendo 3DS',
                           request.META['HTTP_USER_AGENT']) and \
                 not re.match('/blog/mobile', request.path):
             return redirect(request.path.replace('/blog/', '/blog/mobile/', 1))
