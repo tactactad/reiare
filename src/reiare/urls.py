afa566a8323134ca7b56ddacb5dd4ba17077dd07
Takuro Shimoda <tac@reiare.net>
2011-06-10 22:40:01 +0900

    maigrate django 1.3

--- src/reiare/urls.py
+++ src/reiare/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       (r'^admin/(.*)', admin.site.root),
+                       (r'^admin/', include(admin.site.urls)),
                        (r'^blog/', include('reiare.blog.urls')),
 )
 
