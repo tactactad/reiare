73b1e5bfc332ae314c24e882f503210b1c5afd00
Takuro Shimoda <tac@reiare.net>
2011-10-21 17:38:57 +0900

    pjax support to tag's page
    タグ表示関係のページをpjax対応。

--- src/reiare/blog/apis.py
+++ src/reiare/blog/apis.py
@@ -200,6 +200,19 @@ def index(request, page=1):
                               context_instance=RequestContext(request))
 
 
+@pjax_access
+def index_pjax(request, page=1):
+    entries = Entry.published_objects.all()
+    dic, objects = paginator_from_objects_and_num_and_page(entries, 5, page)
+    dic['url'] = '/blog/recents/'
+    return render_to_response('2.0/generic/entry_archive_partial.html',
+                              {'latest': objects,
+                               'lastupdate': objects[0].attr_created(),
+                               'paginator': dic,
+                               'view_mode': 'index'},
+                              context_instance=RequestContext(request))
+
+
 def detail(request, year, month, day, slug):
     entries = entries_from_slug(year, month, day, slug)
     return render_to_response('2.0/generic/entry_archive.html',
@@ -222,6 +235,18 @@ def tag_entries_index(request, tag, page=1):
     tag, entries = tag_and_entries(tag)
     paginator, entries = paginator_from_objects_and_num_and_page(entries, 10, page)
     return render_to_response('2.0/generic/entry_archive_tag.html',
+                              {'object_list': entries,
+                               'tag': tag,
+                               'paginator': paginator,
+                               'view_mode': 'archives'},
+                              context_instance=RequestContext(request))
+
+
+@pjax_access
+def tag_entries_index_pjax(request, tag, page=1):
+    tag, entries = tag_and_entries(tag)
+    paginator, entries = paginator_from_objects_and_num_and_page(entries, 10, page)
+    return render_to_response('2.0/generic/entry_archive_tag_partial.html',
                               {'object_list': entries,
                                'tag': tag,
                                'paginator': paginator,
