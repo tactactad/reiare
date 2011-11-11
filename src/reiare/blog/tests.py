5f889bcac53443d10afc726050e5b0041395c3d9
Takuro Shimoda <tac@reiare.net>
2011-11-11 13:53:20 +0900

    implement pjax url test case and refactoring

--- src/reiare/blog/tests.py
+++ src/reiare/blog/tests.py
@@ -22,23 +22,28 @@ class ReiareExtrasTestCase(TestCase):
 
 class BaseResponseTestCase(TestCase):
 
-    def responseFromClientAndURL(self, client, url, status_code=200):
-        response = client.get(url)
+    def responseFromClientAndURL(self, client, url, data={}, status_code=200):
+        response = client.get(url, data)
         self.statusCodeTest(response, status_code)
         return response
 
     def statusCodeTest(self, response, status_code=200):
         self.assertEqual(response.status_code, status_code)
 
-    def statusCodeTestFromClientAndURL(self, client, url, status_code=200):
-        self.responseFromClientAndURL(client, url, status_code)
+    def statusCodeTestFromClientAndURL(self, client, url, data={}, status_code=200):
+        self.responseFromClientAndURL(client, url, data, status_code)
 
     def templatesTest(self, names, response):
         for i in range(len(response.templates)):
             self.assertEqual(response.templates[i].name, names[i])
 
-    def simplifyResponseTest(self, client, url, templates= [], status_code=200):
-        response = self.responseFromClientAndURL(client, url, status_code)
+    def simplifyResponseTest(self, client, url, data={}, templates=(), status_code=200):
+        response = self.responseFromClientAndURL(client, url, data, status_code)
+        self.templatesTest(templates, response)
+
+    def simplifyPjaxResponseTest(self, client, url, data={}, templates=(), status_code=200):
+        response = client.get(url, data, HTTP_X_PJAX=True)
+        self.statusCodeTest(response, status_code)
         self.templatesTest(templates, response)
 
     def jsonResponseTest(self, client, url, data={}, status_code=200):
@@ -62,20 +67,20 @@ class ResponseTestCase(BaseResponseTestCase):
 
     def testblog(self):
         self.simplifyResponseTest(self.client, '/blog/',
-                                  ['2.0/generic/entry_archive.html',
-                                   '2.0/base.html',
-                                   '2.0/generic/entry_archive_partial.html',
-                                   '2.0/entry.html',
-                                   '2.0/jquery_templates.html',
-                                   '2.0/entry_article_template.html',
-                                   '2.0/common_js.html'])
+                                  templates=('2.0/generic/entry_archive.html',
+                                             '2.0/base.html',
+                                             '2.0/generic/entry_archive_partial.html',
+                                             '2.0/entry.html',
+                                             '2.0/jquery_templates.html',
+                                             '2.0/entry_article_template.html',
+                                             '2.0/common_js.html'))
 
     def testRecentJson(self):
-        self.responseFromClientAndURL(self.client, '/blog/api/recents/1/entry.json', 302)
+        self.responseFromClientAndURL(self.client, '/blog/api/recents/1/entry.json', status_code=302)
         self.jsonResponseTest(self.client, '/blog/api/recents/1/entry.json')
 
     def testApi(self):
-        for url in ['/blog/api/2010/11/05/slug/entry.json',
+        for url in ('/blog/api/2010/11/05/slug/entry.json',
                     '/blog/api/2010/11/1/entry.json',
                     '/blog/api/2010/11/entry.json',
                     '/blog/api/entry/1.json',
@@ -85,74 +90,111 @@ class ResponseTestCase(BaseResponseTestCase):
                     '/blog/api/archives/title.json',
                     '/blog/api/tag/apple/1/entry.json',
                     '/blog/api/tag/apple/entry.json',
-                    '/blog/api/tag/apple/entry.json']:
+                    '/blog/api/tag/apple/entry.json'):
             self.jsonResponseTest(self.client, url)
+        # urls = ['/blog/api/2010/11/05/slug/entry.json',
+        #         '/blog/api/2010/11/1/entry.json',
+        #         '/blog/api/2010/11/entry.json',
+        #         '/blog/api/entry/1.json',
+        #         '/blog/api/recents/1/entry.json',
+        #         '/blog/api/recents/title.json',
+        #         '/blog/api/random/title.json',
+        #         '/blog/api/archives/title.json',
+        #         '/blog/api/tag/apple/1/entry.json',
+        #         '/blog/api/tag/apple/entry.json',
+        #         '/blog/api/tag/apple/entry.json']
+        # map((lambda url: self.jsonResponseTest(self.client, url)), urls)
 
     def testPjax(self):
-        response = self.client.get('/blog/', {},
-                                   HTTP_X_PJAX=True)
-        self.statusCodeTest(response)
-        self.templatesTest(['2.0/generic/entry_archive_partial.html',
-                            '2.0/entry.html'],
-                           response);
+        args = ({'url': '/blog/',
+                'templates': ('2.0/generic/entry_archive_partial.html',
+                              '2.0/entry.html')},
+                {'url': '/blog/2010/11/1/',
+                 'templates': ('2.0/generic/entry_archive_month_partial.html',
+                               '2.0/generic/entry_archive_month_nav.html',
+                               '2.0/generic/entry_archive_partial.html',
+                               '2.0/entry.html',
+                               '2.0/generic/entry_archive_month_nav.html')},
+                {'url': '/blog/2010/11/',
+                 'templates': ('2.0/generic/entry_archive_month_partial.html',
+                               '2.0/generic/entry_archive_month_nav.html',
+                               '2.0/generic/entry_archive_partial.html',
+                               '2.0/entry.html',
+                               '2.0/generic/entry_archive_month_nav.html')},
+                {'url': '/blog/2010/11/05/slug/',
+                 'templates': ('2.0/entry.html',)},
+                {'url': '/blog/recents/1/',
+                 'templates': ('2.0/generic/entry_archive_partial.html',
+                               '2.0/entry.html')},
+                {'url': '/blog/archives/',
+                 'templates': ('2.0/archive_partial.html',)})
+        for arg in args:
+            self.simplifyPjaxResponseTest(self.client, arg['url'], templates= arg['templates'])
+
 
     def testFeeds(self):
-        self.statusCodeTestFromClientAndURL(self.client, '/blog/feeds/latest/', 301)
+        response = self.client.get('/blog/feeds/latest/', follow=True)
+        self.assertEqual(response.redirect_chain[1][1], 302)
+        self.assertEqual(response.redirect_chain[0][0], 'http://feeds.feedburner.com/reiare/cPIq')
         self.simplifyResponseTest(self.client, '/blog/feeds/tag/apple/',
-                                  ['feeds/tag_title.html',
-                                   'feeds/tag_description.html',])
-        self.simplifyResponseTest(self.client, '/blog/feeds_ad/latest/',
-                                  ['feeds/latest_title.html',
-                                   'feeds/latest_description.html',])
+                                  templates=('feeds/tag_title.html',
+                                             'feeds/tag_description.html'))
+        self.simplifyResponseTest(self.client, '/blog/feeds_ad/latest/', {},
+                                  templates=('feeds/latest_title.html',
+                                             'feeds/latest_description.html'))
 
     def testOldBlog(self):
-        self.simplifyResponseTest(self.client, '/blog/1.0/',
-                                  ['blog/entry_archive.html',
-                                   'base.html',
-                                   'entry.html',
-                                   'recent_entries_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/1.0/2010/10/',
-                                  ['blog/entry_archive_month.html',
-                                   'base.html',
-                                   'month_navigation.html',
-                                   'month_navigation.html',
-                                   'recent_entries_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/1.0/2010/11/05/slug/',
-                                  ['blog/entry_detail.html',
-                                   'base.html',
-                                   'entry.html',
-                                   'recent_entries_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/1.0/tag/apple/',
-                                  ['blog/entry_list.html',
-                                   'list_template.html',
-                                   'base.html',
-                                   'recent_entries_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/body/1/',
-                                  ['entry_body.html',
-                                   'entry_comment.html'])
-        self.simplifyResponseTest(self.client, '/blog/recent_entries/1/',
-                                  ['recent_entries_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/more_entries/1/',
-                                  ['more_entries.html',
-                                   'entry.html'])
+        args = ({'url': '/blog/1.0/',
+                 'templates': ('blog/entry_archive.html',
+                               'base.html',
+                               'entry.html',
+                               'recent_entries_box.html')},
+                {'url': '/blog/1.0/2010/10/',
+                 'templates': ('blog/entry_archive_month.html',
+                               'base.html',
+                               'month_navigation.html',
+                               'month_navigation.html',
+                               'recent_entries_box.html')},
+                {'url': '/blog/1.0/2010/11/05/slug/',
+                 'templates': ('blog/entry_detail.html',
+                               'base.html',
+                               'entry.html',
+                               'recent_entries_box.html')},
+                {'url': '/blog/1.0/tag/apple/',
+                 'templates': ('blog/entry_list.html',
+                               'list_template.html',
+                               'base.html',
+                               'recent_entries_box.html')},
+                {'url': '/blog/body/1/',
+                 'templates': ('entry_body.html',
+                               'entry_comment.html')},
+                {'url': '/blog/recent_entries/1/',
+                 'templates': ('recent_entries_box.html',)},
+                {'url': '/blog/more_entries/1/',
+                 'templates': ('more_entries.html',
+                               'entry.html')})
+        for arg in args:
+            self.simplifyResponseTest(self.client, arg['url'], templates=arg['templates'])
 
     def testTouch(self):
-        self.simplifyResponseTest(self.client, '/blog/touch/',
-                                  ['iui_base.html',
-                                   'iui_entry_box.html',
-                                   'iui_entry_comment_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/touch/more_entries/1/',
-                                  ['iui_more_entries.html'])
-        self.simplifyResponseTest(self.client, '/blog/touch/2010/11/05/slug/',
-                                  ['iui_entry.html',
-                                   'iui_entry_box.html',
-                                   'iui_entry_comment_box.html'])
-        self.simplifyResponseTest(self.client, '/blog/touch/tag/test/',
-                                  ['iui_entries_by_tag.html'])
-        self.simplifyResponseTest(self.client, '/blog/touch/tag/1/',
-                                  ['iui_entries_by_tag.html'])
-        self.simplifyResponseTest(self.client, '/blog/touch/tag/test/more_entries/1/',
-                                  ['iui_more_entries.html'])
+        args = ({'url': '/blog/touch/',
+                 'templates': ('iui_base.html',
+                               'iui_entry_box.html',
+                               'iui_entry_comment_box.html')},
+                {'url': '/blog/touch/more_entries/1/',
+                 'templates': ('iui_more_entries.html',)},
+                {'url': '/blog/touch/2010/11/05/slug/',
+                 'templates': ('iui_entry.html',
+                               'iui_entry_box.html',
+                               'iui_entry_comment_box.html')},
+                {'url': '/blog/touch/tag/test/',
+                 'templates': ('iui_entries_by_tag.html',)},
+                {'url': '/blog/touch/tag/1/',
+                 'templates': ('iui_entries_by_tag.html',)},
+                {'url': '/blog/touch/tag/test/more_entries/1/',
+                 'templates': ('iui_more_entries.html',)})
+        for arg in args:
+            self.simplifyResponseTest(self.client, arg['url'], templates=arg['templates'])
 
     def testAsin2Asamashi(self):
         self.responseFromClientAndURL(self.client, 'http://reiare.net/blog/asin2asamashi/spiceoflife04-22/B005119CMA/')
